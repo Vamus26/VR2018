b1293afa9a987ce6e9d7254e6d84b78400b2a716
Lukas Fritz <lukas.fritz@student.tugraz.at>
2021-02-10 00:08:31 +0200

    added all assignments

---
+++ Ass4/Assets/CamScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+
+    public Camera main;
+    public GameObject birdi;
+    public GameObject world;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            main.gameObject.SetActive(!main.gameObject.activeInHierarchy);
+            //  yield return new WaitForEndOfFrame();
+
+            birdi.gameObject.SetActive(!birdi.gameObject.activeInHierarchy);
+            if (birdi.gameObject.activeInHierarchy)
+            {
+                world.transform.SetParent(birdi.transform);
+            }
+            else
+            {
+                world.transform.SetParent(main.transform);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //Debug.Log("hello");
+            world.gameObject.transform.Translate(0, 0, -1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Debug.Log("hello");
+            world.gameObject.transform.Translate(1.0f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //Debug.Log("hello");
+            world.gameObject.transform.Translate(0, 0, 1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //  Debug.Log("hello");
+            world.gameObject.transform.Translate(-1.0f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            world.gameObject.transform.Rotate(-Vector3.down, 10.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            world.gameObject.transform.Rotate(Vector3.down, 10.0f);
+        }
+    }
+}
---
+++ Ass4/Assets/noRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noRotation : MonoBehaviour {
+    Quaternion tmp;
+    Vector3 tmp2;
+	// Use this for initialization
+	void Start () {
+       tmp =  this.gameObject.transform.localRotation;
+        tmp2 = this.gameObject.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.localRotation = tmp;
+        this.gameObject.transform.localPosition = tmp2;
+
+    }
+}
